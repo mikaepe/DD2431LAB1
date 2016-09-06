6d4b57e7ddd730285d4f120f4610252dc5f2f9b8
zardan <sardan@kth.se>
2016-09-06 17:17:45 +0200

    test2

--- lab1.py
+++ lab1.py
@@ -1,10 +1,11 @@
-# lab 1, machine learning
+# lab 1 decision trees, machine learning
 
 import monkdata as m
-from dtree import entropy
+from dtree import entropy, averageGain
 
-ent = [entropy(m.monk1),entropy(m.monk2),entropy(m.monk3)]
-print(ent)
+monkset = [m.monk1, m.monk2, m.monk3]
+
+ent = [entropy(m.monk1), entropy(m.monk2), entropy(m.monk3)]
+print ent
+# assignment 1:
 # answer: 1.0, 0.9571, 0.9998
-
-# sara says hi
