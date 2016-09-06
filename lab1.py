bb53a081df3cea7ef69ed56c2766d0d72a61dc9d
Mikael Persson <mikaepe@kth.se>
2016-09-06 23:25:30 +0200

    /mp

--- lab1.py
+++ lab1.py
@@ -9,3 +9,10 @@ ent = [entropy(m.monk1), entropy(m.monk2), entropy(m.monk3)]
 print ent
 # assignment 1:
 # answer: 1.0, 0.9571, 0.9998
+
+
+# testar lite.... :-)
+avG = averageGain(m.monk1,m.attributes[1])
+print avG
+
+
