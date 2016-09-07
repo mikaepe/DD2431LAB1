953c1049a9a51ef032ec71717f84a7314e1d733c
zardan <sardan@kth.se>
2016-09-07 11:21:29 +0200

    hej

--- lab1.py
+++ lab1.py
@@ -1,27 +1,49 @@
 # lab 1 decision trees, machine learning
 
 import monkdata as m
-from dtree import entropy, averageGain
+from dtree import *
 
 monkset = [m.monk1, m.monk2, m.monk3]
+att = m.attributes
 
 ent = [entropy(m.monk1), entropy(m.monk2), entropy(m.monk3)]
-print ent
+#print ent
 # assignment 1:
 # answer: 1.0, 0.9571, 0.9998
 
-monkGain = []
 
-for data in monkset:
-    # iterates over monksets
-    setGain = []
-    for i in range(len(m.attributes)):
-        # iterates over attributes for each monkset
-        avG = round(averageGain(data,m.attributes[i]),4)
-        setGain.append(avG)
-    monkGain.append(setGain)
+def gainCalc(setList, attributesToIterate):
+    monkGain = []
+    for set in setList:
+        # iterates over monksets
+        setGain = []
 
-for i in range(len(monkGain)):
-    print monkGain[i]
+        # fix attributes to iterate
+        #attributesToIterate = m.attributes
+        #for att in enumerate(attributesToSkip):
+        #    attributesToIterate = attributesToIterate.remove(att)
 
-# hej /Sara
+        for i in range(len(attributesToIterate)):
+            # iterates over attributes for each monkset
+            avG = round(averageGain(set,attributesToIterate[i]),12)
+            setGain.append(avG)
+        monkGain.append(setGain)
+    return monkGain
+
+monkGain = gainCalc(monkset, att)
+
+# for i in range(len(monkGain)):
+#     print monkGain[i]
+
+subli = []
+for aval in m.attributes[4].values:
+    sub = select(m.monk1, m.attributes[4], aval)
+    subli.append(sub)
+
+# update remaining attributes
+att = tuple(x for x in att if x != m.attributes[4])
+
+monkGain2 = gainCalc(subli, att)
+
+for i in range(len(monkGain2)):
+    print monkGain2[i]
