503d3a7f8ab048edc21ec97ad1c2b8b347a6629c
Mikael Persson <mikaepe@kth.se>
2016-09-07 11:20:37 +0200

    lab1old added

---
+++ lab1old.py
@@ -0,0 +1,27 @@
+# lab 1 decision trees, machine learning
+
+import monkdata as m
+from dtree import *
+
+monkset = [m.monk1, m.monk2, m.monk3]
+
+ent = [entropy(m.monk1), entropy(m.monk2), entropy(m.monk3)]
+print ent
+# assignment 1:
+# answer: 1.0, 0.9571, 0.9998
+
+monkGain = []
+
+for data in monkset:
+    # iterates over monksets
+    setGain = []
+    for i in range(len(m.attributes)):
+        # iterates over attributes for each monkset
+        avG = round(averageGain(data,m.attributes[i]),4)
+        setGain.append(avG)
+    monkGain.append(setGain)
+
+for i in range(len(monkGain)):
+    print monkGain[i]
+
+# hej /Sara
